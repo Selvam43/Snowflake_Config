c12767990786d242ee2238c7ae314aa34f99b0eb
Selvam43 <muthuselvam.m99@gmail.com>
2022-07-05 19:50:46 +0530

    onsubmit code for url

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ function App() {
 const handleSubmit = (e) => {
     e.preventDefault();
     const newText= { text };
-    fetch("/", {
+    var URL = document.getElementById("URL").value;
+    console.log(URL);
+    fetch("/api", {
       method: "POST",
       headers: {"Content-Type": "application/JSON"},
       body: JSON.stringify(newText)
@@ -29,40 +31,40 @@ const handleSubmit = (e) => {
   .then((data) => console.log(data));
 }
 
-window.onload=function(){
-var button = document.getElementById('submit');
-      button.addEventListener('click', (e) => {
+// window.onload=function(){
+// var button = document.getElementById('Contained');
+//       button.addEventListener('click', (e) => {
        
-              e.preventDefault();
-              const username = document.getElementsByClassName('URL').value;   
-              console.log("hello's");                
-              fetch('/', {
-                  method: 'POST',
-                  headers: {
-                      Authorization: 'Bearer abcdxyz',
-                      'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({
-                      username,
-                  }),
-              })
-                  .then((res) => {
-                      return res.json();
-                  })
-                  .then((data) => console.log(data));
-          });
-        }
+//               e.preventDefault();
+//               const URL = document.querySelector('#URL').value;   
+//               console.log("hello's");                
+//               fetch('/', {
+//                   method: 'POST',
+//                   headers: {
+//                       // Authorization: 'Bearer abcdxyz',
+//                       'Content-Type': 'application/json',
+//                   },
+//                   body: JSON.stringify({
+//                       URL,
+//                   }),
+//               })
+//                   .then((res) => {
+//                       return res.json();
+//                   })
+//                   .then((data) => console.log(data));
+//           });
+//         }
   return (
     <div className="App">
       <header className="App-header">
       <p className="title">{!data ? "Loading...": data}</p>
       <form onSubmit={handleSubmit}>
-        <UI.TextField style={{margin : "10px"}} id="outlined-basics" className="URL" label="Snowflake Url" variant="outlined"/><br></br>
+        <UI.TextField style={{margin : "10px"}} id="URL"  label="Snowflake Url" variant="outlined"/><br></br>
         <UI.TextField id="outlined-basics" label = "User Name" variant="outlined" />
         <br></br>
         <UI.TextField style={{margin : "10px"}} type="password" id="outlined-basics" label = "Password" variant="outlined" />
         <br></br>
-        <UI.Button style={styleObj} id="Contained submit"  variant="contained" label="Submit">Submit</UI.Button>
+        <UI.Button style={styleObj} id="Contained"  variant="contained" type="submit" label="Submit">Submit</UI.Button>
         </form>
       </header>
       
